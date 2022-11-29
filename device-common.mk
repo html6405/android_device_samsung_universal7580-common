9d379ca247f56764ddc170bfe4126731d2d628d4
html6405 <peter.schelchshorn@mhs-solutions.at>
2022-11-29 10:25:27 +0100

    universal7580-common: device-common.mk: build android.hardware.wifi.hostapd-V1-ndk to for hostapd to fix tethering

--- device-common.mk
+++ device-common.mk
@@ -219,7 +219,8 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf \
     android.hardware.wifi@1.0-service.legacy \
-    android.hardware.wifi.supplicant-V1-ndk
+    android.hardware.wifi.supplicant-V1-ndk \
+    android.hardware.wifi.hostapd-V1-ndk
 
 # Properties
 -include $(LOCAL_PATH)/system_prop.mk
